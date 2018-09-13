9749bda270ff8f137cad4ab1cb3aaaefe0dbface
Jesus Valera <jesus1902@outlook.com>
2018-09-13 20:47:23 +0200

    Render partial, form, submit data via input.

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -9,4 +9,19 @@ class BlogsController < ApplicationController
     @blog = Blog.find(params[:id])
   end
 
+  def new
+
+  end
+
+  def create
+    @blog = Blog.create(params.require(:blog).permit(:title, :content))
+
+    if @blog.save
+      redirect_to root_path
+    else
+      render :new
+    end
+
+  end
+
 end
