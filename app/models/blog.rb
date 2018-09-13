dadab3c123fd0b482a0fd8b85f64e95ae9e9e2ee
Jesus Valera <jesus1902@outlook.com>
2018-09-14 00:21:03 +0200

    Validation and error messages in form.

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,2 +1,6 @@
 class Blog < ApplicationRecord
+
+  validates :title, :content, presence: true
+  validates :title, length: { minimum: 2 }
+  validates :title, uniqueness: { message: 'already taken'}
 end
